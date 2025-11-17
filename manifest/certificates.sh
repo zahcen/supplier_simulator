ba323949da93a8044de964402af9f87ff69ed1b8
Ahcen Mint <ahcen.zitouni@gmail.com>
2025-11-17 19:30:53 +0100

    Mtls

--- manifest/certificates.sh
+++ manifest/certificates.sh
@@ -13,3 +13,24 @@ openssl req -new -key client.key -subj "/CN=myclient" -out client.csr
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 365
 
 k create secret generic supplier-credential-mutual-tls --from-file=tls.crt=server.crt --from-file=tls.key=server.key --from-file=ca.crt=ca.crt --dry-run=client -o yaml > supplier-credential-mutual-tls.yaml
+
+
+#Generate Web client auth certificate
+
+#Generate ca key
+openssl genrsa -out ca-client.key 4096
+
+#Generate ca crt auto signé
+openssl req -x509 -new -nodes -key ca-client.key -sha256 -days 3650 -subj "/CN=CA-CLIENT"   -out ca-client.crt
+
+#Generate client key
+openssl genrsa -out client.key 4096
+
+#Generate csr client
+openssl req -new -key client.key -out client.csr -subj "/CN=wcs-client"
+
+#Generate client.crt
+openssl x509 -req -in client.csr -CA ca-client.crt -CAkey ca-client.key -CAcreateserial -out client.crt -days 36500 -sha256 -extfile client.ext -extensions v3_req
+
+openssl x509 -in client.crt -text | grep -A4 "Extended Key Usage"
+
